7f551a43f30d8081c1eb027a188b89e6784152f8
Fsly <2645358281@qq.com>
2020-06-20 21:18:22 +0800

    6.20提交

--- app/src/main/java/com/example/chronos/BlankFragment.java
+++ app/src/main/java/com/example/chronos/BlankFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +19,33 @@ public class BlankFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_blank, container,
                 false);
+
+        final Animation animation1 = AnimationUtils.loadAnimation(getContext(), R.anim.scale_alpha_anim);
+        final Animation animation2 = AnimationUtils.loadAnimation(getContext(), R.anim.scale_alpha_anim);
+        final Animation animation3 = AnimationUtils.loadAnimation(getContext(), R.anim.scale_alpha_anim);
+        final Animation animation4 = AnimationUtils.loadAnimation(getContext(), R.anim.scale_alpha_anim);
+
+        final FrameLayout scanner = view.findViewById(R.id.layout_scanner);
+        final ImageView circle1=scanner.findViewById(R.id.circle1);
+        final ImageView circle2=scanner.findViewById(R.id.circle2);
+        final ImageView circle3=scanner.findViewById(R.id.circle3);
+        final ImageView circle4=scanner.findViewById(R.id.circle4);
+
+        scanner.findViewById(R.id.start_can).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                circle1.startAnimation(animation1);
+
+                animation2.setStartOffset(600);
+                circle2.startAnimation(animation2);
+
+                animation3.setStartOffset(1200);
+                circle3.startAnimation(animation3);
+
+                animation4.setStartOffset(1800);
+                circle4.startAnimation(animation4);
+            }
+        });
 
         return view;
     }
